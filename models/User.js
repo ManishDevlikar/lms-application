cbf39fbabf0a3d4dd7f1a1ce53921b2d29b29552
manish.Devlikar <manish.devlikar5003@gmail.com>
2022-03-27 18:24:32 +0530

    commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -27,11 +27,11 @@ const userSchema = new mongoose.Schema({
 
 
 // fire a function before doc saved to db
-// userSchema.pre('save', async function (next) {
-//   const salt = await bcrypt.genSalt();
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 // static method to login user
 userSchema.statics.login = async function (email, password) {
